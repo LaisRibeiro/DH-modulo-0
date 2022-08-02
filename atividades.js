94dad1c700f817e0490563dcb06cbd834bf0e9ab
laisdenegri <lais_ribeiro@beirario.com.br>
2022-08-02 17:18:12 -0300

    Atividades de Lógica

---
+++ atividades.js
@@ -0,0 +1,63 @@
+// Lógica JS
+
+// Atividade 1
+let saudacao = "Olá, mundo!"
+
+// Atividade 2
+let idade = 26
+let peso = 62
+
+// Atividade 3
+let eBrasileiro = true
+
+// Atividade 4
+var idade2 = 26
+var sobrenome = "Ribeiro"
+var salarioMinimo = 1.200 
+
+// Atividade 5
+var nome = "Laís"
+var idade3 = 26
+var altura = 1.63
+var possuiPet = true
+var filmesFavoritos = ["Soul", "Capitão Fantástico"]
+
+// Atividade 6
+var nome = "Taylor"
+var sobrenome = "Alvarenga"
+var profissao = "Programadora"
+
+var cartaoDeVisita = (nome +" "+ sobrenome +" - "+ profissao)
+
+// Atividade 7
+let janeiro = 100
+let fevereiro = 300
+let marco = 90
+
+let valorTotal = janeiro + fevereiro + marco
+
+
+// Atividade 8
+let numeroA = 5
+let numeroB = 2
+
+let resultadoSoma = numeroA + numeroB
+let resultadoMultiplicacao = numeroA * numeroB
+let resultadoDivisao = numeroA / numeroB
+let resultadoSubtracao = numeroA - numeroB
+
+// Atividade 9 
+let trabalhoDeHistoria = 8.0
+let trabalhoDeMatematica = 7.0
+let trabalhoDeCiencia = 10
+let trabalhoDeGeografia = 9.5
+let quantidadeDeTrabalhos = 4
+
+let media = (trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia) / quantidadeDeTrabalhos
+
+// Atividade 10
+var numeroMenor = 3
+var numeroMaior = 6
+
+var eMenor = numeroMenor < numeroMaior
+var eMaior = numeroMenor > numeroMaior
