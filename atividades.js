b2fe461a81a54d45a7d9e4e39aae3984c09902a0
laisdenegri <lais_ribeiro@beirario.com.br>
2022-08-08 16:00:59 -0300

    Estrutura de Decisão

--- atividades.js
+++ atividades.js
@@ -61,3 +61,115 @@ var numeroMaior = 6
 
 var eMenor = numeroMenor < numeroMaior
 var eMaior = numeroMenor > numeroMaior
+
+
+// Estrutura de Decisão
+
+// Atividade 1
+let nome = "João"
+let idade1 = 17
+
+if(idade >=18){
+    console.log("Olá, " + nome);
+    console.log("Você passou no nosso teste e já pode dirigir!");
+} else{
+    console.log("Olá, " + nome);
+}
+
+// Atividade 2
+let saldo = 1.500
+
+if(saldo > 0){
+    console.log("Seu saldo está positivo! Gostaria de fazer um investimento?")
+}
+
+if(saldo < 0){
+    console.log("Seu saldo está negativo! Gostaria de fazer um empréstimo?")
+}
+
+// Atividade 3
+var idade2 = 15
+
+if (idade >= 18) {
+    console.log("Você já pode dirigir!")
+} else {
+    console.log("Ops, você ainda não tem a idade mínima para dirigir!")
+}
+
+// Atividade 4
+var idade3 = 40
+
+if (idade > 65) {
+    console.log("Você já pode se aposentar")
+} else {
+    console.log("Você ainda não pode se aposentar")
+}
+
+// Atividade 5
+let estrangeiro = true;
+
+if (estrangeiro) {
+    console.log("Você poderia apresentar seu RNE, por favor?")
+} else {
+    console.log("Você poderia apresentar seu CPF, por favor?")
+}
+
+// Atividade 6
+var numeroDaSorte = 18
+
+if ((numeroDaSorte % 2) == 0) {
+    console.log("Par")
+} else {
+    console.log("Ímpar")
+}
+
+// Atividade 7
+var petPeso = 7
+
+if (petPeso < 4) {
+    console.log("Abaixo do Peso")
+} else if (petPeso > 10) {
+    console.log("Acima do Peso")
+} else {
+    console.log("Peso normal")
+}
+
+// Atividade 8
+var ladosIguais = 0
+
+if (ladosIguais == 3) {
+    console.log("Equilátero")
+} else if (ladosIguais == 2) {
+    console.log("Isósceles")
+} else {
+    console.log("Escaleno")
+}
+
+// Atividade 9
+var produtoQtd = 3
+var produtoAtivo = true
+
+if(produtoQtd > 0 && produtoAtivo){
+    console.log("Você pode finalizar essa compra")
+} else {
+    console.log("Produto não está disponível para compra")
+}
+
+// Atividade 10
+var usuarioAltura = 170
+var usuarioIdade = 21
+
+if(usuarioAltura > 150 || usuarioIdade >= 21){
+    console.log("Você pode subir")
+} else {
+    console.log("Desculpe, você não atende os requisitos para usar o brinquedo")
+}
+
+// Atividade 11
+var diaSemana = 'sabado'
+
+if(diaSemana != "sabado" && diaSemana != "domingo"){
+    console.log("Você pode ir ao banco")
+} else {
+    console.log("O banco está fechado, tente outro dia")
+}
